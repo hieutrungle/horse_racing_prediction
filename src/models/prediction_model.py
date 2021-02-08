0cc3aa30af0cb62551894134190d718e9112376f
Hieu Le <hieult1996@gmail.com>
2021-02-07 18:20:29 -0700

    handle imbalance with SMOTE

--- src/models/prediction_model.py
+++ src/models/prediction_model.py
@@ -16,6 +16,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix
 import lightgbm as lgb
 import catboost as cb
+from imblearn.over_sampling import SMOTE, ADASYN
 
 #plt.style.use('fivethirtyeight')
 
@@ -40,6 +41,16 @@ y_train_top3 = df_train["horse_rank_top3"]
 X_test = df_test[useful_features]
 y_test_top3 = df_test["horse_rank_top3"]
 print("Finished selecting!\n")
+
+# Synthetic Minority Oversampling Technique.
+# SMOTE uses a nearest neighbors algorithm to generate new and 
+# synthetic data we can use for training our model.
+print("Handle imbalanced data...")
+print("original ratio 1/0", len(y_train_top3[y_train_top3 == 1])/ len(y_train_top3[y_train_top3 == 0]))
+sm = ADASYN(random_state=27)
+X_train, y_train_top3 = sm.fit_resample(X_train, y_train_top3)
+print("after upsampling, ratio 1/0: ", len(y_train_top3[y_train_top3 == 1])/ len(y_train_top3[y_train_top3 == 0]))
+print("Finished Handling Imbalance!\n")
 
 # Standard Normalization
 print("Normalization...")
@@ -116,7 +127,7 @@ lr_model = linear_model.LogisticRegression()
 
 t0 = time.time()
 lr_model.fit(X_train_std, y_train_top3)
-print(f"Training time for top3 logistic regression classifier: {time.time() - t0} sec\n")
+print(f"Training time for top3 logistic regression classifier: {time.time() - t0} sec")
 lr_predict_top3 = lr_model.predict(X_test_std)
 show_accuracy(y_test_top3, lr_predict_top3)
 
@@ -128,20 +139,21 @@ rf_model = RandomForestClassifier()
 t0 = time.time()
 rf_model.fit(X_train_std, y_train_top3)
 t3 = time.time() - t0
-print(f"Training time for top3 random forest classifier: {time.time() - t0} sec\n")
+print(f"Training time for top3 random forest classifier: {time.time() - t0} sec")
 rf_predict_top3 = rf_model.predict(X_test_std)
 show_accuracy(y_test_top3, rf_predict_top3)
 
 
 # *** CatBoost ***
+# class_weights = (1, 3.5),
 print("Catboost...")
 cb_model = cb.CatBoostClassifier(eval_metric="AUC", depth=10, iterations= 500, 
-                                  l2_leaf_reg= 9, learning_rate= 0.15, class_weights = (1, 3.5),
+                                  l2_leaf_reg= 9, learning_rate= 0.15, 
                                   silent=True)
 
 t0 = time.time()
 cb_model.fit(X_train_std,y_train_top3)
-print(f"Training time for top3 CatBoost classifier: {time.time() - t0} sec\n")
+print(f"Training time for top3 CatBoost classifier: {time.time() - t0} sec")
 cb_predict_top3 = cb_model.predict(X_test_std)
 # Round the float number
 cb_predict_top3 = cb_predict_top3.round(0)
@@ -150,17 +162,17 @@ cb_predict_top3 = cb_predict_top3.astype(int)
 show_accuracy(y_test_top3, cb_predict_top3)
 
 
-# *** LightGBM ***
+# *** LightGBM *** , "is_unbalance": True
 print("LightGBM...")
 lg_model = lgb.LGBMClassifier(silent=False)
 
 t0 = time.time()
 d_train = lgb.Dataset(X_train_std, label=y_train_top3)
 params = {"max_depth": 50, "learning_rate" : 0.1, "num_leaves": 300,  "n_estimators": 300,
-          'objective': 'binary', "metrics": "auc", "is_unbalance": True}
+          'objective': 'binary', "metrics": "auc"}
 lg_model = lgb.train(params, d_train)
 t5 = time.time() - t0
-print(f"Training time for top3 LightGBM classifier: {time.time() - t0} sec\n")
+print(f"Training time for top3 LightGBM classifier: {time.time() - t0} sec")
 lg_predict_top3 = lg_model.predict(X_test_std)
 # Round the float number
 lg_predict_top3 = lg_predict_top3.round(0)
