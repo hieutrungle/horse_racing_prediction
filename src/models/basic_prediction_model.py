f9d3fe496a290c45be3b880c0fc18348ab0807c6
Hieu Le <hieult1996@gmail.com>
2021-02-10 12:02:25 -0700

    add basic and adcanced models

--- src/models/basic_prediction_model.py
+++ src/models/basic_prediction_model.py
@@ -4,9 +4,9 @@ warnings.filterwarnings('ignore')
 
 # Import libraries
 import json
+import os
 from os.path import dirname, abspath
 from sklearn import linear_model
-from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score
 import pandas as pd
@@ -14,8 +14,6 @@ import pandas as pd
 import time
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix
-import lightgbm as lgb
-import catboost as cb
 from imblearn.over_sampling import SMOTE, ADASYN
 from hyperopt import hp, fmin, tpe, STATUS_OK, Trials
 from sklearn.model_selection import cross_val_score
@@ -63,23 +61,53 @@ X_train_std = std_scaler.transform(X_train)
 X_test_std = std_scaler.transform(X_test)
 print("Finished normalization!\n")
 
-def show_accuracy(groundtruth, prediction):
+"""# Useful functions:
+
+## Accuracy calculation and pickling models
+
+- def calc_accuracy: calculate and show accuracy
+
+- def create_model_pickle: create a pickle file that contains the model architecture
+"""
+def calc_accuracy(groundtruth, prediction):
     # input: groundtruth and prediction results
     # output: f1_score and comfussion matrix
     f1_score_model = f1_score(groundtruth, prediction, average="weighted")
     print(f"F1 score: {f1_score_model:0.5f}")
     cm = confusion_matrix(groundtruth, prediction)
-    print(f"correct_pred_top3/total_actual_top3: {cm[1,1]/(cm[1,0]+cm[1,1]):0.5f}")
-    return f1_score, cm
+    true_pos_win_rate = cm[1,1]/(cm[1,0]+cm[1,1])
+    print(f"correct_pred_top3/total_actual_top3: {true_pos_win_rate:0.5f}")
+    return f1_score_model, true_pos_win_rate
 
 def create_model_pickle(model, filename):
+    # create a pickle file that contains the model architecture
     print(f"Pickling the model into the file {filename}")
-    with open(filename, 'wb') as f:
+    with open("./output_models/" + filename, 'wb') as f:
         pickle.dump(model, f)
     print("Finished pickling the model")
 
+"""
+    Import f1_scores and true_pos_wining_rate if exist, otherwise create new dictionaries
+"""
+
+if os.path.exists('./score/f1_scores.json') == True:
+    with open("./score/f1_scores.json", 'r') as f:
+        f1_scores = json.load(f)
+else:
+    f1_scores = {'train':{'logis_reg': 0, 'randomForest': 0, 'catboost': 0, 'lightGBM': 0, 'EN_HARD': 0, 'EN_SOFT': 0},
+             'test':{'logis_reg': 0, 'randomForest': 0, 'catboost': 0, 'lightGBM': 0, 'EN_HARD': 0, 'EN_SOFT': 0}}
+    true_pos_wining_rates = {'train':{'logis_reg': 0, 'randomForest': 0, 'catboost': 0, 'lightGBM': 0, 'EN_HARD': 0, 'EN_SOFT': 0},
+                                'test':{'logis_reg': 0, 'randomForest': 0, 'catboost': 0, 'lightGBM': 0, 'EN_HARD': 0, 'EN_SOFT': 0}}
+
+if os.path.exists('./score/true_pos_wining_rates.json') == True:
+    with open("./score/true_pos_wining_rates.json", 'r') as f:
+        true_pos_wining_rates = json.load(f)
+else:
+    true_pos_wining_rates = {'train':{'logis_reg': 0, 'randomForest': 0, 'catboost': 0, 'lightGBM': 0, 'EN_HARD': 0, 'EN_SOFT': 0},
+                            'test':{'logis_reg': 0, 'randomForest': 0, 'catboost': 0, 'lightGBM': 0, 'EN_HARD': 0, 'EN_SOFT': 0}}
 
 """ Prediction """
+
 # *** Logistic Regression ***
 print("Logistic Regression...")
 fit_intercept = {0: True, 1: False}
@@ -92,7 +120,6 @@ lr_space = {'tol': hp.uniform('tol', 1e-6, 1e-3),
 
 def lr_objective(params):
     model = linear_model.LogisticRegression(**params)
-    
     acc = cross_val_score(model, X_train_std, y_train_top3, cv = 4, scoring='f1').mean()
 
     # We aim to maximize accuracy, therefore we return it as a negative value
@@ -103,22 +130,44 @@ lr_trials = Trials()
 lr_best_hyperparams = fmin(fn= lr_objective,
                 space= lr_space,
                 algo= tpe.suggest,
-                max_evals = 80,
+                max_evals = 100,
                 trials= lr_trials)
 print(f"elapse time of tuning hyperparameter process: {time.time()-t_tuning} sec")
 lr_best_hyperparams['fit_intercept']= fit_intercept[lr_best_hyperparams['fit_intercept']]
 lr_best_hyperparams['solver']= solver[lr_best_hyperparams['solver']]
 print("best hyperparameters for logistic regression: ", lr_best_hyperparams)
-lr_model = linear_model.LogisticRegression(C=lr_best_hyperparams['C'],
-                                        tol=lr_best_hyperparams['tol'],
-                                        fit_intercept=lr_best_hyperparams['fit_intercept'],
-                                        solver=lr_best_hyperparams['solver'])
+# Dump the best parameters to json file for later use
+with open('./best_hyperparameters/logis_best_hyperparams.json', 'w') as fp:
+    json.dump(lr_best_hyperparams, fp)
 
+# lr_model = linear_model.LogisticRegression(C=lr_best_hyperparams['C'],
+#                                         tol=lr_best_hyperparams['tol'],
+#                                         fit_intercept=lr_best_hyperparams['fit_intercept'],
+#                                         solver=lr_best_hyperparams['solver'])
+lr_model = linear_model.LogisticRegression()
 t0 = time.time()
+lr_model.set_params(**lr_best_hyperparams)
 lr_model.fit(X_train_std, y_train_top3)
 print(f"Training time for top3 logistic regression classifier: {time.time() - t0} sec")
-lr_predict_top3 = lr_model.predict(X_test_std)
-show_accuracy(y_test_top3, lr_predict_top3)
+
+# Prediction on training dataset
+lr_predict_top3_train = lr_model.predict(X_train_std)
+
+# Prediction on testing dataset
+lr_predict_top3_test = lr_model.predict(X_test_std)
+
+# Calculate accuracy
+print("Accuracy for training set:")
+f1_score_train, true_pos_win_rate_train = calc_accuracy(y_train_top3, lr_predict_top3_train)
+print("Accuracy for testing set:")
+f1_score_test, true_pos_win_rate_test = calc_accuracy(y_test_top3, lr_predict_top3_test)
+
+# Append values to accuracy dictionary
+f1_scores["train"]["logis_reg"] = round(f1_score_train, 4)
+true_pos_wining_rates["train"]["logis_reg"] = round(true_pos_win_rate_train, 4)
+f1_scores["test"]["logis_reg"] = round(f1_score_test, 4)
+true_pos_wining_rates["test"]["logis_reg"] = round(true_pos_win_rate_test, 4)
+
 create_model_pickle(lr_model, 'logis_model.pkl')
 print("Logistic Regression Done!\n")
 
@@ -127,13 +176,15 @@ print("Logistic Regression Done!\n")
 # *** Random Forest ***
 print("Random Forest...")
 crit = {0: 'entropy', 1: 'gini'}
-feat = {0: 'sqrt', 2: 'log2'}
-est = {0: 10, 2: 150, 3: 300}
+feat = {0: 'sqrt', 1: 'log2'}
+est = {0: 50, 1: 150, 2: 300}
 rf_space = {
     "n_estimators": hp.choice("n_estimators", est.values()),
-    "max_depth": hp.quniform("max_depth", 5, 1000,10),
+    "max_depth": hp.quniform("max_depth", 5, 400,10),
     "criterion": hp.choice("criterion", crit.values()),
-    'max_features': hp.choice('max_features', feat.values())
+    'max_features': hp.choice('max_features', feat.values()),
+    'min_samples_split': hp.randint('min_samples_split', 500),
+    'min_samples_leaf': hp.randint('min_samples_leaf', 300)
 }
 
 def rf_objective(params):
@@ -147,27 +198,47 @@ rf_trials = Trials()
 rf_best_hyperparams = fmin(fn= rf_objective,
                 space= rf_space,
                 algo= tpe.suggest,
-                max_evals = 10,
+                max_evals = 20,
                 trials= rf_trials)
 print(f"elapse time of tuning hyperparameter process: {time.time()-t_tuning} sec")
-
-rf_best_hyperparams = { 'criterion': crit[rf_best_hyperparams['criterion']], 
-                        'max_depth': rf_best_hyperparams['max_depth'], 
-                        'max_features': feat[rf_best_hyperparams['max_features']],
-                        'n_estimators': est[rf_best_hyperparams['n_estimators']]
-}
-
 print("best hyperparameters for Random Forest: ", rf_best_hyperparams)
 
-rf_model = RandomForestClassifier(criterion = rf_best_hyperparams['criterion'], 
-                                    max_depth = rf_best_hyperparams['max_depth'], 
-                                    max_features = rf_best_hyperparams['max_features'],
-                                    n_estimators = rf_best_hyperparams['n_estimators']
-                                      )
+rf_best_hyperparams['criterion'] = crit[rf_best_hyperparams['criterion']]
+rf_best_hyperparams['max_features'] = feat[rf_best_hyperparams['max_features']]
+rf_best_hyperparams['n_estimators'] = est[rf_best_hyperparams['n_estimators']]
+rf_best_hyperparams['min_samples_split'] = int(rf_best_hyperparams['min_samples_split'])
+rf_best_hyperparams['min_samples_leaf'] = int(rf_best_hyperparams['min_samples_leaf'])
+print("best hyperparameters for Random Forest: ", rf_best_hyperparams)
+# Dump the best parameters to json file for later use
+with open('./best_hyperparameters/ranforest_best_hyperparams.json', 'w') as fp:
+    json.dump(rf_best_hyperparams, fp)
+
+rf_model = RandomForestClassifier(n_jobs=-1)
+rf_model.set_params(**rf_best_hyperparams)
 t0 = time.time()
 rf_model.fit(X_train_std,y_train_top3)
 print(f"Training time for top3 random forest classifier: {time.time() - t0} sec")
-rf_predict_top3 = rf_model.predict(X_test_std)
-show_accuracy(y_test_top3, rf_predict_top3)
+
+# Prediction on training dataset
+rf_predict_top3_train = rf_model.predict(X_train_std)
+# Prediction on testing dataset
+rf_predict_top3_test = rf_model.predict(X_test_std)
+# Calculate accuracy
+print("Accuracy for training set:")
+f1_score_train, true_pos_win_rate_train = calc_accuracy(y_train_top3, rf_predict_top3_train)
+print("Accuracy for testing set:")
+f1_score_test, true_pos_win_rate_test = calc_accuracy(y_test_top3, rf_predict_top3_test)
+
+# Append values to accuracy dictionary
+f1_scores["train"]["randomForest"] = round(f1_score_train, 4)
+true_pos_wining_rates["train"]["randomForest"] = round(true_pos_win_rate_train, 4)
+f1_scores["test"]["randomForest"] = round(f1_score_test, 4)
+true_pos_wining_rates["test"]["randomForest"] = round(true_pos_win_rate_test, 4)
+
+with open('./score/f1_scores.json', 'w') as fp:
+    json.dump(f1_scores, fp)
+with open('./score/true_pos_wining_rates.json', 'w') as fp:
+    json.dump(true_pos_wining_rates, fp)
+
 create_model_pickle(rf_model, 'random_forest_model.pkl')
 print("Random Forest Done!\n")
---
+++ src/models/final_model.py
@@ -0,0 +1,162 @@
+
+# Hide warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+# Import libraries
+import json
+import os
+from os.path import dirname, abspath
+from sklearn.metrics import f1_score
+import pandas as pd
+import time
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+import lightgbm as lgb
+import catboost 
+#from imblearn.over_sampling import SMOTE, ADASYN
+from hyperopt import hp, fmin, tpe, STATUS_OK, Trials
+from sklearn.model_selection import cross_val_score
+import pickle
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import linear_model
+from sklearn.utils import class_weight
+import numpy as np
+from sklearn.ensemble import VotingClassifier
+
+"""## Import data"""
+
+parent_dir_path = dirname(dirname(abspath(__file__)))
+print(parent_dir_path)
+df_train = pd.read_csv(parent_dir_path +"/extract_feature/training.csv")
+df_test = pd.read_csv(parent_dir_path +"/extract_feature/testing.csv")
+
+df = pd.concat([df_train, df_test], ignore_index=True)
+
+"""## Select features based upon those algorithm and personal analysis"""
+
+print("Select useful data...")
+with open(parent_dir_path + '/models/useful_features.json') as f:
+  useful_features = json.load(f)
+# Remove SexCD attribute
+useful_features.pop("SexCD")
+
+X = df[useful_features]
+y_top3 = df["horse_rank_top3"]
+print("Finished selecting!\n")
+
+"""# Normalization
+
+Standard Normalization
+"""
+
+print("Normalization...")
+std_scaler = StandardScaler()
+std_scaler.fit(X)
+X_std = std_scaler.transform(X)
+print("Finished normalization!\n")
+
+"""# Useful functions:
+
+## Accuracy calculation and pickling models
+
+- def calc_accuracy: calculate and show accuracy
+
+- def create_model_pickle: create a pickle file that contains the model architecture
+"""
+def calc_accuracy(groundtruth, prediction):
+    # input: groundtruth and prediction results
+    # output: f1_score and comfussion matrix
+    f1_score_model = f1_score(groundtruth, prediction, average="weighted")
+    print(f"F1 score: {f1_score_model:0.5f}")
+    cm = confusion_matrix(groundtruth, prediction)
+    true_pos_win_rate = cm[1,1]/(cm[1,0]+cm[1,1])
+    print(f"correct_pred_top3/total_actual_top3: {true_pos_win_rate:0.5f}")
+    return f1_score_model, true_pos_win_rate
+
+def create_model_pickle(model, filename):
+    print(f"Pickling the model into the file {filename}")
+    with open("./output_models/" + filename, 'wb') as f:
+        pickle.dump(model, f)
+    print("Finished pickling the model")
+
+
+with open("./best_hyperparameters/catboost_best_hyperparams.json", 'r') as f:
+    catboost_best_hyperparams = json.load(f)
+
+with open("./best_hyperparameters/lgb_best_hyperparams.json", 'r') as f:
+    lgb_best_hyperparams = json.load(f)
+
+# CatBoost
+catboost_model = catboost.CatBoostClassifier()
+catboost_model.set_params(**catboost_best_hyperparams)
+t0 = time.time()
+catboost_model.fit(X_std, y_top3)
+print(f"Training time for top3 CatBoost classifier: {time.time() - t0} sec")
+
+# Prediction on training dataset
+catboost_predict_top3 = catboost_model.predict(X_std)
+# Round the float number
+catboost_predict_top3 = catboost_predict_top3.round(0)
+#converting from float to integer
+catboost_predict_top3 = catboost_predict_top3.astype(int)
+
+# Calculate accuracy
+f1_score_train, true_pos_win_rate_train = calc_accuracy(y_top3, catboost_predict_top3)
+
+# Create a pickle file that contains the model
+create_model_pickle(catboost_model, 'catboost_final.pkl')
+cm = confusion_matrix(y_top3, catboost_predict_top3)
+print(cm)
+print("CatBoost Done!\n")
+
+# class_name = [0, 1]
+# plot_confusion_matrix(catboost_predict_top3, y_top3, class_name, normalize=False)
+
+# LightGBM
+lgb_model = lgb.LGBMClassifier()
+lgb_model.set_params(**lgb_best_hyperparams)
+t0 = time.time()
+lgb_model.fit(X_std, y_top3)
+print(f"Training time for top3 CatBoost classifier: {time.time() - t0} sec")
+
+# Prediction on training dataset
+lgb_predict_top3 = lgb_model.predict(X_std)
+# Round the float number
+lgb_predict_top3 = lgb_predict_top3.round(0)
+#converting from float to integer
+lgb_predict_top3 = lgb_predict_top3.astype(int)
+
+# Calculate accuracy
+f1_score_train, true_pos_win_rate_train = calc_accuracy(y_top3, lgb_predict_top3)
+
+# Create a pickle file that contains the model
+create_model_pickle(lgb_model, 'lightGBM_final.pkl')
+cm = confusion_matrix(y_top3, lgb_predict_top3)
+print(cm)
+print("LightGBM Done!\n")
+
+
+"""## Ensemble model
+
+### Soft Voting
+"""
+print("Soft Voting Ensemble...")
+#create a dictionary of our models
+estimator = [("catboost", catboost_model), 
+             ("lightGBM", lgb_model)]
+
+#create our voting classifier, inputting our models
+
+vot_soft = VotingClassifier(estimators = estimator, voting ='soft') 
+vot_soft.fit(X_std, y_top3)
+
+yhat = vot_soft.predict(X_std)
+
+# Calculate accuracy
+f1_score, true_pos_win_rate = calc_accuracy(y_top3, yhat)
+
+create_model_pickle(vot_soft, 'vot_soft_final.pkl')
+cm = confusion_matrix(y_top3, lgb_predict_top3)
+print(cm)
+print("Soft Voting Ensemble Done!")
